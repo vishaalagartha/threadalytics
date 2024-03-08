7fc14058fd2fd3bd8f826d3a51b6245ac7e4332e
Vishaal Agartha <vishaalagartha@gmail.com>
2024-03-07 18:03:22 -0800

    Fixes parsing

--- server/src/utils/db.js
+++ server/src/utils/db.js
@@ -1,5 +1,7 @@
 import AWS from 'aws-sdk'
 import * as csv from '@fast-csv/parse'
+import * as fs from 'fs'
+import * as path from 'path'
 
 const s3 = new AWS.S3({
   apiVersion: '2006-03-01',
@@ -9,10 +11,10 @@ const s3 = new AWS.S3({
   secretAccessKey: process.env.SECRET_ACCESS_KEY
 })
 
-const parseCSV = async (csvFile, delimiter = ',') => {
+const parseCSV = async (csvFile, delimiter = ';') => {
   let data = []
   return new Promise((resolve, reject) => {
-    csv.parseStream(csvFile, { headers: true, delimiter })
+    csv.parseStream(csvFile, { headers: true, delimiter, quote: null })
       .on('data', (d) => {
         data.push(d)
       })
@@ -23,6 +25,7 @@ const parseCSV = async (csvFile, delimiter = ',') => {
         reject(error)
       })    
   })
+  
 }
 
 const getBucketContents = async (key, delimiter = ',') => {
